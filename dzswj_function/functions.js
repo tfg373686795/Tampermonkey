6aff6c72020cfae8b004a7ef1471f25518c92b52
tfg373686795 <373686795@qq.com>
2019-09-18 10:52:57 +0800

    添加本地脚本

---
+++ dzswj_function/functions.js
@@ -0,0 +1,23 @@
+alert("初始化加载");
+
+var addStyle = function(cssText) {
+	var head = document.querySelector('head');
+	var style = document.createElement('style');
+	style.setAttribute('type', 'text/css');
+	style.textContent = cssText;
+	head.appendChild(style);
+};
+var addScript = function(jsText) {
+	var head = document.querySelector('head');
+	var script = document.createElement('script');
+	script.setAttribute('type', 'text/javascript');
+	script.textContent = jsText;
+	head.appendChild(script);
+};
+var addScriptForUrl = function(jsUrl) {
+	var head = document.querySelector('head');
+	var script = document.createElement('script');
+	script.type="text/javascript";
+	script.src=jsUrl;
+	head.appendChild(script);
+};
